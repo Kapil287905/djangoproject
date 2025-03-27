64f20d876c31bb42596621160c8e732d1aa3d16c
kapil287905 <kmeghnani28@gmail.com>
2025-03-27 09:13:21 +0530

    djabgo project

--- libararyproject/app/views.py
+++ libararyproject/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse, redirect
 from .models import Book
 from datetime import datetime
 # Create your views here.
@@ -20,3 +20,24 @@ def index(req):
     print(hour)
     context = {"myname":myname,"curdate":curdate, "hour":hour,"allbooks":allbooks}
     return render(req,"index.html", context)
+
+def singup(req):    
+    if req.method == "GET":
+        print(req.method) # GET
+        return render(req,"singup.html")
+    else:
+        print(req.method) # POST
+        uname=req.POST["uname"]
+        upass=req.POST["upass"]
+        ucpass=req.POST["ucpass"]
+        print(uname,upass,ucpass)
+        if upass != ucpass:
+            errmsg="Password and Confirm password nust be same"
+            context = {"errmsg":errmsg}
+            return render(req,"singup.html",context)
+        else:
+            return redirect("singin")
+        
+
+def singin(req):
+    return render(req, "singin.html")
