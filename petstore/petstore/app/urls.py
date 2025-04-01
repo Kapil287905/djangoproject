cbc7266637a4c970f406125ac25c8a48648b8e31
kapil287905 <kmeghnani28@gmail.com>
2025-04-01 08:58:44 +0530

    petsrore project

---
+++ petstore/petstore/app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("",views.index,name="index"),
+    path("singup/",views.singup,name="singup"),
+    path("singin/",views.singin,name="singin"),
+    path("dashboard/",views.dashboard,name="dashboard"),
+    path("userlogout/",views.userlogout,name="userlogout")
+]
