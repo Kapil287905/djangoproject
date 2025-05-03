8e38ba29c49bf2231476553d27eeecb8ac81a3db
kapil287905 <kmeghnani28@gmail.com>
2025-05-03 09:21:21 +0530

    rest_api

--- finalproject/movieapiproject/app/models.py
+++ finalproject/movieapiproject/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+    title=models.CharField(max_length=30)
+    rating=models.PositiveIntegerField()
+    duration=models.CharField(max_length=40)
+    type=(('sci-fi','sci-if'),('social','social'),('bio','bio'))
+    category=models.CharField(max_length=50,choices=type)
+    photo=models.URLField()
